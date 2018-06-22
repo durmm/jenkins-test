5ce5b242f37d71e27f04830f73ea4f0c029e4043
durmm <gaya.stdev@gmail.com>
2018-06-22 16:11:36 +0400

    Add driver class

---
+++ config/Drivers.java
@@ -0,0 +1,79 @@
+import java.io.File;
+
+public class Drivers {
+
+    private String getOSName() {
+        return System.getProperty("os.name");
+    }
+
+    private String getOSArch() {
+        return System.getProperty("os.arch");
+    }
+
+    private String getUserDirectory() {
+        return System.getProperty("user.dir");
+    }
+
+    private String getOSDriverDirectory() {
+        String os = getOSName().toLowerCase();
+        if (os.contains("windows")) {
+            return "windows";
+        } else if (os.contains("linux")) {
+            return "linux";
+        } else {
+            System.out.println("Current device OS is not included in the written project.");
+            return "rien";
+        }
+    }
+
+    public String getDriverPath(char browser) {
+        String osArch = getOSArch();
+        String os = getOSDriverDirectory();
+        String separator = File.separator;
+        String driverDirectory = getUserDirectory() + "src" + separator + "main" + separator + "resources"
+                + separator + "webdrivers" + separator + os + separator;
+        if (os.equals("linux")) {
+            switch (browser) {
+                case 'F':
+                    if (osArch.contains("64")) {
+                        return driverDirectory + "geckodriver-v0.21.0-linux64";
+                    } else if (osArch.contains("32") || osArch.contains("86")) {
+                        return driverDirectory + "geckodriver-v0.21.0-linux32";
+                    }
+                case 'C':
+                    return driverDirectory + "chromedriver-v2.40-linux64";
+                case 'O':
+                    return driverDirectory + "operadriver-v2.36-linux64";
+                default:
+                    System.out.println("");
+                    return "rien";
+            }
+        } else if (os.equals("windows")) {
+            switch (browser) {
+                case 'F':
+                    return driverDirectory + "geckodriver-v0.21.0-win64.exe";
+                case 'C':
+                    return driverDirectory + "chromedriver-v2.40-win32.exe";
+                case 'O':
+                    if (osArch.contains("64")) {
+                        return driverDirectory + "operadriver-v2.36-win64.exe";
+                    } else if (osArch.contains("32") || osArch.contains("86")) {
+                        return driverDirectory + "operadriver-v2.36-win32.exe";
+                    }
+                case 'E':
+                    return driverDirectory + "edgedriver-v17134.exe";
+                case 'I':
+                    if (osArch.contains("64")) {
+                        return driverDirectory + "iedriver-v3.9.0-win64.exe";
+                    } else if (osArch.contains("32") || osArch.contains("86")) {
+                        return driverDirectory + "iedriver-v3.9.0-win32.exe";
+                    }
+                default:
+                    System.out.println("");
+                    return "rien";
+            }
+        } else {
+            return "rien";
+        }
+    }
+}
