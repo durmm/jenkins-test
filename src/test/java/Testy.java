bdbdd92dc1cdd05b5bc8672e5af9f1ff7e080c5a
durmm <gaya@stdevmail.com>
2018-08-03 17:57:26 +0400

    Upgrade selenium dependency

--- src/test/java/Testy.java
+++ src/test/java/Testy.java
@@ -1,16 +1,13 @@
+import config.Drivers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-/**
- * Created by durmm on 22/06/18.
- */
 public class Test {
     private Drivers driver = new Drivers();
 
-    @org.testng.annotations.Test()
-    private void test() {
+    public void test() {
         WebDriver webDriver = new FirefoxDriver();
         System.setProperty("webdriver.gecko.driver", driver.getDriverPath('F'));
         webDriver.get("google.ru");
